ef338ad3521b21388be916ba6d7f8f65584111cd
Logan Wilson <loganwilson777@gmail.com>
2023-01-05 16:22:54 -0700

    add second method

--- friends.rb
+++ friends.rb
@@ -1,7 +1,13 @@
 numbers = [34,15,30,56,49,31,13,61]
 
+# numbers.each do |number|
+#     if number > 30
+#     puts number
+#     end
+# end
+
 numbers.each do |number|
-    if number > 30
+    if number < 60
     puts number
     end
 end
